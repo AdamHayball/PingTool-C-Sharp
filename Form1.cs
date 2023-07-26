511727f0c41f593614144af493b1462076e175c1
AdamHayball <140003236+AdamHayball@users.noreply.github.com>
2023-07-26 11:57:15 -0400

    Beta v1.1.0

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PingTool.NET
@@ -10,55 +12,76 @@ namespace PingTool.NET
         private TextBox outputTextBox;
         private TextBox textBoxGatewayIP;
         private TextBox textBoxUsableIP;
+        private TextBox textBoxNumPings; // Added the textBoxNumPings field
         private Button buttonRunPing;
         private Button buttonReset;
-        // Declare the labels as class-level fields
         private Label labelUsableIP;
         private Label labelGatewayIP;
+        private Label labelNumPings; // Added the labelNumPings field
         public Form1()
         {
-            InitializeComponent(); // Initialize the components on the form
+            InitializeComponent();
+            textBoxNumPings.Text = "200"; // Set the default value of the number of pings
+
+            // Get the version number from assembly information
+            string version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            int firstDotIndex = version.IndexOf(".");
+            int secondDotIndex = version.IndexOf(".", firstDotIndex + 1);
+            string minorVersion = version.Substring(0, secondDotIndex);
+            this.Text = $"Ping Tool v{minorVersion}";
         }
 
-        private void runPingButton_Click(object sender, EventArgs e) // Changed the event handler name to 'buttonRunPing_Click'
+        private async void runPingButton_Click(object sender, EventArgs e)
         {
             string usableIP = textBoxUsableIP.Text.Trim();
             string gatewayIP = textBoxGatewayIP.Text.Trim();
 
-            // Clear the output text box
+            if (!int.TryParse(textBoxNumPings.Text, out int numPings))
+            {
+                MessageBox.Show("Please enter a valid number for the number of pings.");
+                return;
+            }
+
             outputTextBox.Text = "Pinging IPs. Please Wait...";
 
-            // Validate the input to ensure at least one valid IP address is provided
             bool validIPProvided = false;
+            StringBuilder resultBuilder = new StringBuilder();
+
             if (System.Net.IPAddress.TryParse(usableIP, out _))
             {
                 validIPProvided = true;
-                // Run the script for the usable IP
-                outputTextBox.Text += Environment.NewLine + $"Results for Usable IP ({usableIP}):" + Environment.NewLine;
-                outputTextBox.Text += RunPing(usableIP);
+                resultBuilder.AppendLine($"Results for Usable IP ({usableIP}):");
+
+                // Perform the pinging asynchronously using Task.Run
+                string pingResults = await Task.Run(() => RunPing(usableIP, numPings));
+                resultBuilder.AppendLine(pingResults);
             }
 
             if (System.Net.IPAddress.TryParse(gatewayIP, out _))
             {
                 validIPProvided = true;
-                // Add a separator
-                if (outputTextBox.Text.Length > 0)
+                if (resultBuilder.Length > 0)
                 {
-                    outputTextBox.Text += Environment.NewLine + "=================";
+                    resultBuilder.AppendLine("=================");
                 }
-                // Run the script for the gateway IP
-                outputTextBox.Text += Environment.NewLine + $"Results for Gateway IP ({gatewayIP}):" + Environment.NewLine;
-                outputTextBox.Text += RunPing(gatewayIP);
+                resultBuilder.AppendLine($"Results for Gateway IP ({gatewayIP}):");
+
+                // Perform the pinging asynchronously using Task.Run
+                string pingResults = await Task.Run(() => RunPing(gatewayIP, numPings));
+                resultBuilder.AppendLine(pingResults);
             }
 
             if (!validIPProvided)
             {
-                // No valid IP address provided
                 outputTextBox.Text = "Please enter at least one valid IP address.";
+            }
+            else
+            {
+                outputTextBox.Text = resultBuilder.ToString();
             }
         }
 
-        private string RunPing(string ipAddress)
+        private string RunPing(string ipAddress, int numPings)
         {
             string pingResults = "";
             var pingSender = new Ping();
@@ -66,7 +89,7 @@ namespace PingTool.NET
             var latencies = new List<long>();
             bool timeoutMessageAdded = false;
 
-            for (int i = 0; i < 200; i++)
+            for (int i = 0; i < numPings; i++)
             {
                 var reply = pingSender.Send(ipAddress, 1000, new byte[32], pingOptions);
 
@@ -84,13 +107,14 @@ namespace PingTool.NET
                 }
             }
 
-            // Calculate and display statistics
+            // Calculate and display statistics based on the actual 'numPings'
             int received = latencies.Count;
-            int lost = 200 - received;
-            double lossPercentage = (lost / 200.0) * 100.0;
+            int lost = numPings - received;
+            double lossPercentage = (lost / (double)numPings) * 100.0;
 
             pingResults += Environment.NewLine + $"Ping statistics for {ipAddress}:" + Environment.NewLine;
-            pingResults += $"    Packets: Sent = 200, Received = {received}, Lost = {lost} (Loss Percentage = {lossPercentage:F2}%)" + Environment.NewLine;
+            pingResults += $"    Packets: Sent = {numPings}, Received = {received}, Lost = {lost} (Loss Percentage = {lossPercentage:F2}%)" + Environment.NewLine;
+
 
             if (received > 0)
             {
@@ -119,9 +143,8 @@ namespace PingTool.NET
             return pingResults;
         }
 
-        private void resetButton_Click(object sender, EventArgs e) // Changed the event handler name to 'buttonReset_Click'
+        private void resetButton_Click(object sender, EventArgs e)
         {
-            // Clear the text boxes and output
             textBoxUsableIP.Text = "";
             textBoxGatewayIP.Text = "";
             outputTextBox.Text = "";
@@ -172,32 +195,59 @@ namespace PingTool.NET
             // 
             // buttonRunPing
             // 
-            this.buttonRunPing.Location = new System.Drawing.Point(10, 100);
+            this.buttonRunPing.Location = new System.Drawing.Point(10, 140);
             this.buttonRunPing.Name = "buttonRunPing";
             this.buttonRunPing.Size = new System.Drawing.Size(100, 30);
             this.buttonRunPing.TabIndex = 2;
             this.buttonRunPing.Text = "Run Ping";
             this.buttonRunPing.UseVisualStyleBackColor = true;
-            this.buttonRunPing.Click += new System.EventHandler(this.runPingButton_Click); // Changed the event handler reference to 'runPingButton_Click'
-                                                                                           // 
-                                                                                           // buttonReset
-                                                                                           // 
-            this.buttonReset.Location = new System.Drawing.Point(120, 100);
+            this.buttonRunPing.Click += new System.EventHandler(this.runPingButton_Click);
+            this.Controls.Add(this.buttonRunPing);
+            // Changed the event handler reference to 'runPingButton_Click'                                                            
+            //                                                             
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(120, 140);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(100, 30);
             this.buttonReset.TabIndex = 3;
             this.buttonReset.Text = "Reset";
             this.buttonReset.UseVisualStyleBackColor = true;
-            this.buttonReset.Click += new System.EventHandler(this.resetButton_Click); // Changed the event handler reference to 'resetButton_Click'
-                                                                                       // 
-                                                                                       // outputTextBox
-                                                                                       // 
-            this.outputTextBox.Location = new System.Drawing.Point(10, 140);
+            this.buttonReset.Click += new System.EventHandler(this.resetButton_Click);
+            this.Controls.Add(this.buttonReset);
+            // 
+            // labelNumPings
+            // 
+            this.labelNumPings = new System.Windows.Forms.Label();
+            this.labelNumPings.AutoSize = true;
+            this.labelNumPings.Location = new System.Drawing.Point(10, 100);
+            this.labelNumPings.Name = "labelNumPings";
+            this.labelNumPings.Size = new System.Drawing.Size(96, 13);
+            this.labelNumPings.Text = "Number of Pings:";
+            this.Controls.Add(this.labelNumPings);
+            // Add the labelNumPings control to the Form1
+            this.Controls.Add(this.labelNumPings);
+            // 
+            // textBoxNumPings
+            // 
+            this.textBoxNumPings = new System.Windows.Forms.TextBox();
+            this.textBoxNumPings.Location = new System.Drawing.Point(200, 100);
+            this.textBoxNumPings.Name = "textBoxNumPings";
+            this.textBoxNumPings.Size = new System.Drawing.Size(150, 20);
+            this.textBoxNumPings.TabIndex = 5;
+            this.Controls.Add(this.textBoxNumPings);
+            // Changed the event handler reference to 'resetButton_Click'                                                         
+            // 
+            // outputTextBox                                               
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(10, 180);
             this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
             this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.outputTextBox.Size = new System.Drawing.Size(380, 200);
             this.outputTextBox.TabIndex = 4;
+            this.Controls.Add(this.outputTextBox);
             // 
             // Form1
             // 
@@ -209,8 +259,13 @@ namespace PingTool.NET
             this.Controls.Add(this.buttonRunPing);
             this.Controls.Add(this.textBoxGatewayIP);
             this.Controls.Add(this.textBoxUsableIP);
-            this.Controls.Add(this.labelGatewayIP); // Add the labelGatewayIP control to the Form1
-            this.Controls.Add(this.labelUsableIP); // Add the labelUsableIP control to the Form1
+            // Add the labelGatewayIP control to the Form1
+            this.Controls.Add(this.labelGatewayIP); 
+            // Add the labelUsableIP control to the Form1
+            this.Controls.Add(this.labelUsableIP); 
+            // Add the label and textbox controls to the form
+            this.Controls.Add(this.labelNumPings);
+            this.Controls.Add(this.textBoxNumPings);
             this.Name = "Form1";
             this.Text = "Ping Tool";
             this.ResumeLayout(false);
